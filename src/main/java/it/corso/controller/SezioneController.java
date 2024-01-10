740d9eca26555788df01fb4be2e10be9751d1490
MariaRosariaFalco <mrfalco90@gmail.com>
2024-01-10 12:58:26 +0100

    Form funzionanti

--- src/main/java/it/corso/controller/SezioneController.java
+++ src/main/java/it/corso/controller/SezioneController.java
@@ -24,10 +24,14 @@ public class SezioneController {
 	 private ArticoloService articoloService;
 	
 	@GetMapping
-	public String getPage(Model model, @RequestParam(name="richiesta", required=false) String richiesta, @RequestParam("categoria")String categoria) {
+	public String getPage(Model model, @RequestParam(name="richiesta", required=false) String richiesta, @RequestParam(name="categoria", required=false)String categoria 
+			//, @RequestParam(name="id", required=false) Integer o int id
+			) {
 		
 	List<Articolo> risultati = richiesta==null ? articoloService.getArticoliByCategoria(categoria): articoloService.getArticoloByTagInCategoria(richiesta, categoria);
-		
+	//come trasmetto la parola del tag per recuperare id dell'articolo? come passo la categoria? per ora messa come opzionale perché andava in errore
+	//model.addAttribute("id", id);
+	model.addAttribute(categoria);
     model.addAttribute("risultati", risultati);
 		
 		
